1df23b605ab29c5c252d3236301984eaf11b284b
sumitra <sumitrasahoopuja@gmail.com>
2026-01-06 16:28:35 +0530

    intial

---
+++ Sumitra_82/Day2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Sumitra from "./assets/Sumitra"
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="bg-amber-300 text-5xl flex-3">my name is sumitra</h1>
+     </Sumitra>
+    </>
+  )
+}
+
+export default App
---
+++ Sumitra_82/Day2/src/assets/Sumitra.jsx
@@ -0,0 +1,6 @@
+function Sumitra(){
+    return(<>
+            <h1 className="bg-red-700 text-6xl">My name is Sumitra</h1>       
+        </>)
+}
+export default Sumitra;
