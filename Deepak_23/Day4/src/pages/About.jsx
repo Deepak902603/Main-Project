27f9c2ed959903c4e5b83e8a1a8840b2942f0f61
Deepak902603 <deepakrath902@gmail.com>
2026-01-09 17:56:25 +0530

    Day 4 : Implemented new features and updated folder struture and also learn the Router and Naving Concept

---
+++ Deepak_23/Day4/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About(){
+    return(<>
+    <div className="page">
+     <h1>About Page</h1>
+     <p>This is a Demonstration of routing and navigation in React Mr.Biswajit Sir</p>
+     <br />
+     <h2>Thanks for Visiting this Page</h2>
+    </div>
+    </>)
+}
+export default About;
---
+++ Deepak_23/Day4/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return(<>
+    <div className="page">
+     <h1>Contact page</h1>
+     <p>Gmail me at deepakrath902@gmail.com .</p>
+    </div>
+    
+    </>)
+}
+export default Contact;
