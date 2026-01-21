05015de748b0d39a0fa8eac33affaeee3e7e1379
Suparna-1234 <routsuparna900@gmail.com>
2026-01-21 20:36:20 +0530

    day13

---
+++ Suparna_83/day13/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import PerformanceTable from "./components/PerformanceTable";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">React Performance Optimization</h1>
+      <PerformanceTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ Suparna_83/day13/vite-project/src/components/PerformanceTable.jsx
@@ -0,0 +1,27 @@
+function PerformanceTable() {
+  return (
+    <div className="table-container">
+      <table>
+        <tr>
+          <th className="box one">
+            Suparna<br />Rout
+          </th>
+          <th className="box two">
+            Gift<br />Bhubaneswar
+          </th>
+          <th className="box three">
+            age-22
+          </th>
+          <th className="box four">
+            roll no-83
+          </th>
+          <th className="box five">
+            regd no-2405432091
+          </th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default PerformanceTable;
