2e39c34b96ca377f2b3a06613405e6fa902ec70b
Priyanka Behera <priyankabehera8878@gmail.com>
2026-01-27 21:36:24 +0530

    Day15 work added

---
+++ priyanka_48/DAY15/ci_cd/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Day 15</h1>
+      <p>GitHub Actions CI workflow setup</p>
+    </div>
+  );
+}
+
+export default App;
