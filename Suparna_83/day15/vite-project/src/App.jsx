5637fa516dc63f3a1e18f90dc546211d60f61c89
Suparna-1234 <routsuparna900@gmail.com>
2026-02-04 15:03:20 +0530

    day15

---
+++ Suparna_83/day15/vite-project/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Day 15 – Mock API Demo</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
